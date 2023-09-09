3d1d666bc592c36a31fbb0a8cce6500354d2f9dd
Oluleke Moses <adetleke@gmail.com>
2023-09-09 23:51:24 +0100

    Pushing files

---
+++ ATM Python/function/01_function.py
@@ -0,0 +1,11 @@
+name = []
+
+def get_name():
+    new_name = input("What is your name?: ")
+    return new_name
+
+
+namew = get_name()
+name.append(namew)
+
+print(name) # Printing out names that have been stored in name list
---
+++ ATM Python/print.py
@@ -0,0 +1,10 @@
+# Add two numbers, save the result in answer and print out the result
+
+number1 = input("Enter first num: ")
+num2 = input("Enter second number: ")
+
+converted_num1 = int(number1)
+converted_num2 = int(num2)
+
+answer = converted_num1 + converted_num2
+print("Result is:", answer)
