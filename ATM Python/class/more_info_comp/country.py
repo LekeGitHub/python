0413b6cfb9cc849a810df46583ad55bf22b507f0
Oluleke Moses <adetleke@gmail.com>
2023-10-02 07:58:12 +0100

    Python

---
+++ ATM Python/class/more_info_comp/country.py
@@ -0,0 +1,18 @@
+# Be able to print different funtion or method depending on the insert details
+
+community = ["Yoruba", "Hausa", "Igbo", "Igala", "Tiv"]
+
+class country():
+    def __init__(self, name="", continent="", population="", community=""):
+        self.name = name
+        self.continent = continent
+        self.pupulation = population
+        self.commuinity = community
+
+
+    def sercives:()
+        if self.commuinity is not community:
+            
+
+
+
---
+++ Youtube/input_method.py
@@ -0,0 +1,13 @@
+# We are going to use the input method to get data into out code.
+
+# we start by creating a variable name, Let's call it "num"
+
+num = input("Enter a number: ") # This line asks user to enter a number
+
+# We need to convert the input into a number
+
+num = int(num)
+
+print(f"Num * 2 = {num*2}")
+
+
